51132e9bb8a753a68177406eb9856fc978095f40
Ami9a-Bibi <ami9abibi@gmail.com>
2025-03-18 20:22:02 +0500

    Setup SQLite database with SQLAlchemy

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from fastapi import FastAPI
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, func
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 app = FastAPI()
+
+# Database setup
+DATABASE_URL = "sqlite:///./shortener.db"
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
