9192b283a40cafa749528bd27b66f0d97a80b56d
Ami9a-Bibi <ami9abibi@gmail.com>
2025-03-18 20:24:39 +0500

    Add URL model and migrations

--- main.py
+++ main.py
@@ -9,3 +9,15 @@ DATABASE_URL = "sqlite:///./shortener.db"
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+class URL(Base):
+    __tablename__ = "urls"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    url = Column(String, nullable=False)
+    shortCode = Column(String, unique=True, index=True, nullable=False)
+    createdAt = Column(DateTime, default=func.now())
+    updatedAt = Column(DateTime, default=func.now(), onupdate=func.now())
+    visit_count = Column(Integer, default=0)
+
+Base.metadata.create_all(bind=engine)
